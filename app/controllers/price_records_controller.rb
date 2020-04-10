e4b9fe3fa6fe76c7a58a64b9ad51d65d09ee9e0b
Jian Weihang <tonytonyjan@gmail.com>
2020-04-10 12:38:23 +0800

    refactor expiration detection

--- app/controllers/price_records_controller.rb
+++ app/controllers/price_records_controller.rb
@@ -8,13 +8,7 @@ class PriceRecordsController < ApplicationController
       .search_by_friends_order_by_price(
         Array(params[:friends]).map { |friend| PriceRecord::Friend.new(friend[:island], friend[:resident]) }
       ).to_a
-    price_records.select! do |price_record|
-      time_with_zone = price_record.created_at.localtime(price_record.timezone)
-      case time_with_zone.hour
-      when 8...12 then Time.now < Time.new(time_with_zone.year, time_with_zone.month, time_with_zone.day, 12, 0, 0, price_record.timezone)
-      when 12...22 then Time.now < Time.new(time_with_zone.year, time_with_zone.month, time_with_zone.day, 22, 0, 0, price_record.timezone)
-      end
-    end
+    price_records.reject!(&:expired?)
     price_records.map! do |price_record|
       price_record.attributes.slice('id', 'island', 'resident', 'price', 'created_at')
     end
--- app/models/price_record.rb
+++ app/models/price_record.rb
@@ -4,7 +4,7 @@ class PriceRecord < ApplicationRecord
   SECONDS_10_HOURS = 36_000
   Friend = Struct.new(:island, :resident)
 
-  def self.search_by_friends_order_by_price(friends)
+  def self.search_by_friends_order_by_price(friends, now: Time.now)
     where(
       Arel::Nodes::In.new(
         Arel::Nodes::Grouping.new(
@@ -23,7 +23,16 @@ class PriceRecord < ApplicationRecord
         end
       )
     )
-      .where('created_at > ?', Time.now - SECONDS_10_HOURS)
+      .where('created_at > ?', now - SECONDS_10_HOURS)
       .order(price: :desc)
+  end
+
+  def expired?(now: Time.now)
+    time_with_zone = created_at.localtime(timezone)
+    case time_with_zone.hour
+    when 8...12 then now >= Time.new(time_with_zone.year, time_with_zone.month, time_with_zone.day, 12, 0, 0, timezone)
+    when 12...22 then now >= Time.new(time_with_zone.year, time_with_zone.month, time_with_zone.day, 22, 0, 0, timezone)
+    else false
+    end
   end
 end
