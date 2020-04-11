155ce2bceda1972dc87bc2fd1d74cdaa64e84b89
Jian Weihang <tonytonyjan@gmail.com>
2020-04-11 12:12:18 +0800

    refine code

--- app/javascript/packs/app.jsx
+++ app/javascript/packs/app.jsx
@@ -2,17 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "components/App";
 
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/sw.js")
-    .then((reg) => {
-      // registration worked
-      console.log("Registration succeeded. Scope is " + reg.scope);
-    })
-    .catch((error) => {
-      // registration failed
-      console.log("Registration failed with " + error);
-    });
-}
+if ("serviceWorker" in navigator) navigator.serviceWorker.register("/sw.js");
 
 ReactDOM.render(<App />, document.querySelector("#app"));
