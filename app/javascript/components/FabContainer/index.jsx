4c41bd1ac0ddf416c31bc551583f70709cb74274
Jian Weihang <tonytonyjan@gmail.com>
2020-04-19 12:41:22 +0800

    make desktop friendly

--- app/javascript/components/FabContainer/index.jsx
+++ app/javascript/components/FabContainer/index.jsx
@@ -4,9 +4,12 @@ import "./style";
 export default ({ children }) => (
   <div className="fab-container">
     <div className="fab-container__fabs">
-      {React.Children.map(Array(children), (child) => (
+      {React.Children.map(children, (child) => (
         <div className="fab-container__fab">{child}</div>
       ))}
     </div>
+    {React.Children.map(children, () => (
+      <div className="fab-container__spacing" />
+    ))}
   </div>
 );
