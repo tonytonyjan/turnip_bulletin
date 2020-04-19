3f5d22b6491a0a765f06ce3c05986ee2047aea36
Jian Weihang <tonytonyjan@gmail.com>
2020-04-19 13:15:47 +0800

    remove unused code

--- app/javascript/components/History/index.jsx
+++ app/javascript/components/History/index.jsx
@@ -9,18 +9,6 @@ import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
-
 const createPredictUrl = (priceRecords) => {
   const prices = new Array(13);
   for (let i = priceRecords.length - 1; i >= 0; i--) {
