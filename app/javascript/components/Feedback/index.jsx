6317bea86847fc1acd1c81e9675c7885eb48b55a
Jian Weihang <tonytonyjan@gmail.com>
2020-05-03 16:10:44 +0800

    improve iframe scrolling

--- app/javascript/components/Feedback/index.jsx
+++ app/javascript/components/Feedback/index.jsx
@@ -8,10 +8,8 @@ export default ({ onMount }) => {
   return (
     <iframe
       src="https://docs.google.com/forms/d/e/1FAIpQLSeP1L5QxAyPYUhumA1XaZ9QzhR2zQDqpEd7BZjIDlum7WnyPA/viewform?embedded=true"
-      style={{
-        width: "100%",
-        height: "calc(100vh - 148px)",
-      }}
+      width="100%"
+      height="1328"
       frameBorder="0"
       marginHeight="0"
       marginWidth="0"
